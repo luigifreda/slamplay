93a216e901b023c93b6244605fe8b2c311205e20
Luigi Freda <luigifreda@gmail.com>
2024-12-18 16:21:21 +0100

    Added header

--- utils/eigen/EigenMathUtils.h
+++ utils/eigen/EigenMathUtils.h
@@ -1,11 +1,15 @@
-/*
- * This file is part of slamplay
+// *************************************************************************
+/* 
+ * This file is part of the slamplay project.
  * Copyright (C) 2018-present Luigi Freda <luigifreda at gmail dot com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * any later version, at your option. If this file is a modified/adapted 
+ * version of an original file distributed under a different license that 
+ * is not compatible with the GNU General Public License, the 
+ * BSD 3-Clause License will apply instead.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,10 +17,10 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
-
+// *************************************************************************
 #pragma once
 
 #include <fstream>
--- utils/io/Log.h
+++ utils/io/Log.h
@@ -1,3 +1,26 @@
+// *************************************************************************
+/* 
+ * This file is part of the slamplay project.
+ * Copyright (C) 2018-present Luigi Freda <luigifreda at gmail dot com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version, at your option. If this file is a modified/adapted 
+ * version of an original file distributed under a different license that 
+ * is not compatible with the GNU General Public License, the 
+ * BSD 3-Clause License will apply instead.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+// *************************************************************************
 #pragma once
 
 #include <cstring>
